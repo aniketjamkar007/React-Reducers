0fd556c7fcd629bb5acd39613b7bf3f158a85aba
aniketjamkar007 <aniketjamkar@gmail.com>
2021-11-11 16:20:08 +0530

    third commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,7 +6,7 @@ import Button from '../UI/Button/Button';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
-    return { value: action.value, isValid: action.val.includes('@') };
+    return { value: action.val, isValid: action.val.includes('@') };
   }
   if (action.type === 'INPUT_BLUR') {
     return { value: state.value, isValid: state.value.includes('@') };
